16f11f0e31de7151c3a34b93bfe0b414cc6a8b83
Kayla Ondracek and Michael Albers <michael@>
2018-01-16 11:58:34 -0800

    add new form that takes user input and turns it into an array.

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,21 @@ $(document).ready(function() {
     var grocList = [$("#food1").val(), $("#food2").val(), $("#food3").val()];
     var sortList = grocList.slice().sort().toString().toUpperCase().split(",");
     console.log(sortList);
-
     $(this).hide();
     $("#listOutput").html("<ul id='grocItems'></ul>");
-
     sortList.forEach(function(item) {
       $("#grocItems").append("<li>" + item + "</li>");
     });
-
-
-
     event.preventDefault();
   });
+  $("#sentence").submit(function(event) {
+    var sentenceList = $("#food4").val().split(" ")
+    var largeSentence = sentenceList.map(function(list) {
+      if (list.length > 3) {
+        return list;
+      }
+    })
+    console.log(largeSentence);
+    event.preventDefault();
+  })
 });
