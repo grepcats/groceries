767acb1a2458f60e59903c889e374bbb9cc71282
Kayla Ondracek and Michael Albers <kayla@>
2018-01-16 11:36:10 -0800

    create form for grocery list, sort/capitalize entries and output in a list

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
-  
+  $("#groceries").submit(function(event) {
+    var grocList = [$("#food1").val(), $("#food2").val(), $("#food3").val()];
+    var sortList = grocList.slice().sort().toString().toUpperCase().split(",");
+    console.log(sortList);
+
+    $(this).hide();
+    $("#listOutput").html("<ul id='grocItems'></ul>");
+
+    sortList.forEach(function(item) {
+      $("#grocItems").append("<li>" + item + "</li>");
+    });
+
+
+
+    event.preventDefault();
+  });
 });
