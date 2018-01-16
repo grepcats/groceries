6b6bd2fca4c1fb53b196b05f0ad0de33f6d5fa58
Kayla Ondracek and Michael Albers <kayla@>
2018-01-16 15:33:35 -0800

    add ability to add more fields to the grocery list

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,44 @@
 $(document).ready(function() {
+  var grocCount = 3;
+  var grocID = ["groc1", "groc2", "groc3"];
+  $("#more-input-button").click(function(event) {
+    grocCount++
+    $("#more-fields").append("<div class='form-group'><input type='text' id='groc" + grocCount + "' class='form-control'></div>");
+
+    grocID.push("groc" + grocCount.toString());
+    event.preventDefault();
+
+  });
+
   $("#groceries").submit(function(event) {
-    var grocList = [$("#food1").val(), $("#food2").val(), $("#food3").val()];
+    var grocList = grocID.map(function(groc){
+      return $("#" + groc).val();
+
+    });
+
     var sortList = grocList.slice().sort().toString().toUpperCase().split(",");
-    console.log(sortList);
-    $(this).hide();
+    //console.log(sortList);
+    //$(this).hide();
     $("#listOutput").html("<ul id='grocItems'></ul>");
     sortList.forEach(function(item) {
-      $("#grocItems").append("<li>" + item + "</li>");
+      if (item != "") {
+        $("#grocItems").append("<li>" + item + "</li>");
+      } else {
+      }
     });
     event.preventDefault();
   });
 
-  $("#sentence").submit(function(event) {
-    var sentenceList = $("#food4").val().split(" ")
-    console.log(sentenceList);
-    var largeSentence = sentenceList.filter(function(word) {
-      return word.length > 3;
-    });
 
-    $("#listOutput").html("<p>" + largeSentence.reverse().join(" ") + "</p>");
-
-    event.preventDefault();
-  })
+  // $("#sentence").submit(function(event) {
+  //   var sentenceList = $("#food4").val().split(" ")
+  //   console.log(sentenceList);
+  //   var largeSentence = sentenceList.filter(function(word) {
+  //     return word.length > 3;
+  //   });
+  //
+  //   $("#listOutput").html("<p>" + largeSentence.reverse().join(" ") + "</p>");
+  //
+  //   event.preventDefault();
+  // })
 });
